25e4243763d25c4adb7bf1c3ffbc2cc89d47cc68
Vusal Durmushov <007331@ibar.int>
2025-05-29 16:53:59 +0400

    fix types

--- frontEnd/src/MonitoringApp/Pages/Users/Edit/About.tsx
+++ frontEnd/src/MonitoringApp/Pages/Users/Edit/About.tsx
@@ -1,31 +1,34 @@
 import type { TUser } from "@/types";
 
-function About({ user }:TUser) {
-  console.log(name)
+type TAbout = {
+  user?: TUser | null;
+};
+
+function About({ user }: TAbout) {
   const userAbout = {
     "TAM AD": user?.name,
     "İSTİFADƏÇİ ADI": user?.username,
     "E-MAIL": user?.email,
-    "ƏLAVƏ OLUNUB": user?.dateForCreated
+    "ƏLAVƏ OLUNUB": user?.dateForCreated,
   };
 
   return (
     <div>
-      <h1 className='font-medium pb-4'>HAQQINDA</h1>
+      <h1 className="font-medium pb-4">HAQQINDA</h1>
 
       {Object.entries(userAbout).map(([key, value]) => (
-        <div key={key} className='flex flex-col py-2'>
-          <h2 className='text-sm font-medium text-gray-800'>{key}</h2>
+        <div key={key} className="flex flex-col py-2">
+          <h2 className="text-sm font-medium text-gray-800">{key}</h2>
 
           {key === "E-MAIL" ? (
             <a
               href={`mailto:${value}`}
-              className='text-sm text-blue-600 hover:underline'
+              className="text-sm text-blue-600 hover:underline"
             >
               {value}
             </a>
           ) : (
-            <p className='text-sm text-gray-500'>{value}</p>
+            <p className="text-sm text-gray-500">{value}</p>
           )}
         </div>
       ))}
--- frontEnd/src/types.ts
+++ frontEnd/src/types.ts
@@ -8,4 +8,5 @@ export type TUser = {
   createdAt?: string;
   profilImage?: string;
   email?: string;
+  dateForCreated?: string;
 };
