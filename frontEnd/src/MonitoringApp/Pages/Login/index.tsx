1851c7edc32cf396d5d7e64a7a895c9cff54059b
Vusal Durmushov <007331@ibar.int>
2025-06-03 09:11:25 +0400

    createLogin page

--- frontEnd/src/MonitoringApp/Pages/Login/index.tsx
+++ frontEnd/src/MonitoringApp/Pages/Login/index.tsx
@@ -1,10 +1,42 @@
+import { Button } from "@/components/ui/button";
+import InputForm from "@/shared/InputForm";
+import { FormProvider, useForm, type SubmitHandler } from "react-hook-form";
 
 function Login() {
-  return <div>
-    <h1>Monitoring App</h1>
-    <p>Welocome back! Login into your account</p>
+  type TSubmit = {
+    name: string;
+    password: string;
+  };
+  const methods = useForm<TSubmit>();
 
-  </div>;
+  const { handleSubmit } = methods;
+
+  const onSubmit: SubmitHandler<TSubmit> = (data) => console.log(data);
+
+  return (
+    <FormProvider {...methods}>
+      <div className="h-screen flex items-center justify-center bg-gray-50">
+        <div className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md">
+          <h1 className="text-2xl font-bold mb-2 text-center">
+            Monitoring App
+          </h1>
+          <p className="text-gray-600 mb-6 text-center">
+            Welcome back! Login into your account
+          </p>
+
+          <InputForm name="name" label="Fullname" />
+          <InputForm name="password" label="Password" />
+          <Button
+            onClick={handleSubmit(onSubmit)}
+            type="submit"
+            className="w-full mt-4"
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+    </FormProvider>
+  );
 }
 
 export default Login;
